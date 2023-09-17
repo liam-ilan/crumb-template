951a24eaf80d74e265dbb63fc9ee5879f62c0968
Liam Ilan <snowboardsheep@gmail.com>
2023-09-16 19:52:18 -0700

    build-crumb.sh now quieter, set advice.detachtedHead on git clone to false.

--- build-crumb.sh
+++ build-crumb.sh
@@ -3,7 +3,7 @@
 tag="v0.0.1"
 into=".tmp~crumb~inflate"
 
-git clone --branch $tag --depth 1 https://github.com/liam-ilan/crumb.git $into
+git clone -c advice.detachedHead=false --branch $tag --depth 1 https://github.com/liam-ilan/crumb.git $into
 cd $into || exit
 gcc src/*.c -Wall -lm -o crumb 
 cd .. 
